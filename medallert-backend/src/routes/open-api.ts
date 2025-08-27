ea3ad5bfb12fc53296af742e749740dce09c2702
Fabio Freitas <fabiohbritofreitas@protonmail.com>
2025-08-27 15:55:31 -0300

    chore: fix swagger path

--- medallert-backend/src/routes/open-api.ts
+++ medallert-backend/src/routes/open-api.ts
@@ -31,7 +31,7 @@ export function configureOpenAPIDocs(honoServerInstance: Hono) {
   );
 
   honoServerInstance.get(
-    "/",
+    "/swagger",
     basicAuth({ username: "dev", password: "dev" }),
     swaggerUI({ url: "/openapi" }),
   );
