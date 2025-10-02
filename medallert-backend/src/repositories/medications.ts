1303368986ced05c0ad957783ed2b3a9cc2259f0
Fabio Freitas <fabiohbritofreitas@protonmail.com>
2025-10-02 14:52:17 -0300

    feat: make medication sound and visual type optional

--- medallert-backend/src/repositories/medications.ts
+++ medallert-backend/src/repositories/medications.ts
@@ -2,9 +2,70 @@ import { prisma } from "../infra/prisma/client.js";
 import type { PrismaClient } from "../infra/prisma/generated/prisma/index.js";
 
 export type Medication = {
-    medicationId: string;
+  medicationId: string;
+  userId: string;
+  treatmentId: string | null;
+  name: string;
+  dose: string | null;
+  description: string | null;
+  visualTypeId: string | null;
+  soundTypeId: string | null;
+  alertPeriodInHours: number;
+  endTreatmentAt: Date | null;
+};
+
+export interface MedicationRepository {
+  findMedication(id: string): Promise<Medication | null>;
+  findAllMedications(userId: string): Promise<Medication[]>;
+  addMedication(newMedication: {
     userId: string;
-    treatmentId: string | null;
+    treatmentId?: string | null;
+    name: string;
+    dose: string | null;
+    description: string | null;
+    visualTypeId: string | null;
+    soundTypeId: string | null;
+    alertPeriodInHours: number;
+    endTreatmentAt: Date | null;
+  }): Promise<Medication | null>;
+  updateMedication(
+    id: string,
+    updateMedication: {
+      treatmentId?: string | null;
+      name?: string | null;
+      dose?: string | null;
+      description?: string | null;
+      visualTypeId?: string | null;
+      soundTypeId?: string | null;
+      alertPeriodInHours?: number | null;
+      endTreatmentAt?: Date | null;
+    },
+  ): Promise<Medication | null>;
+  deleteMedication(id: string): Promise<Medication | null>;
+}
+
+class PrismaMedicationRepository implements MedicationRepository {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async findMedication(id: string): Promise<Medication | null> {
+    return this.prisma.medications.findUnique({
+      where: {
+        medicationId: id,
+      },
+    });
+  }
+
+  async findAllMedications(userId: string): Promise<Medication[]> {
+    return this.prisma.medications.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+  }
+
+  async addMedication(newMedication: {
+    treatmentId?: string | null;
+    userId: string;
     name: string;
     dose: string | null;
     description: string | null;
@@ -12,100 +73,45 @@ export type Medication = {
     soundTypeId: string;
     alertPeriodInHours: number;
     endTreatmentAt: Date | null;
+  }): Promise<Medication | null> {
+    return await this.prisma.medications.create({
+      data: {
+        ...newMedication,
+      },
+    });
+  }
+
+  async updateMedication(
+    id: string,
+    updateMedication: {
+      treatmentId?: string | null;
+      name?: string | null;
+      dose?: string | null;
+      description?: string | null;
+      visualTypeId?: string | null;
+      soundTypeId?: string | null;
+      alertPeriodInHours?: number | null;
+      endTreatmentAt?: Date | null;
+    },
+  ): Promise<Medication | null> {
+    const { ...data } = updateMedication;
+    const updateData = Object.fromEntries(
+      Object.entries(data).filter(([_, v]) => v !== undefined),
+    );
+
+    return this.prisma.medications.update({
+      where: { medicationId: id },
+      data: updateData,
+    });
+  }
+
+  async deleteMedication(id: string): Promise<Medication | null> {
+    return this.prisma.medications.delete({
+      where: { medicationId: id },
+    });
+  }
 }
 
-export interface MedicationRepository {
-    findMedication(id: string): Promise<Medication | null>;
-    findAllMedications(userId: string): Promise<Medication[]>;
-    addMedication(newMedication: {
-        userId: string;
-        treatmentId?: string | null;
-        name: string;
-        dose: string | null;
-        description: string | null;
-        visualTypeId: string;
-        soundTypeId: string;
-        alertPeriodInHours: number;
-        endTreatmentAt: Date | null;
-    }): Promise<Medication | null>;
-    updateMedication(id: string, 
-        updateMedication: {
-        treatmentId?: string | null;
-        name?: string | null;
-        dose?: string | null;
-        description?: string | null;
-        visualTypeId?: string | null;
-        soundTypeId?: string | null;
-        alertPeriodInHours?: number | null;
-        endTreatmentAt?: Date | null;
-    }): Promise<Medication | null>;
-    deleteMedication(id: string): Promise<Medication | null>;
-}
-
-class PrismaMedicationRepository implements MedicationRepository {
-    constructor(private readonly prisma: PrismaClient) {}
-
-    async findMedication(id: string): Promise<Medication | null> {
-        return this.prisma.medications.findUnique({
-            where: {
-                medicationId: id
-            },
-        });
-    }
-
-    async findAllMedications(userId: string): Promise<Medication[]> {
-        return this.prisma.medications.findMany({
-            where: {
-                userId: userId
-            }
-        });
-    }
-
-    async addMedication(newMedication: {
-        treatmentId?: string | null;
-        userId: string;
-        name: string;
-        dose: string | null;
-        description: string | null;
-        visualTypeId: string;
-        soundTypeId: string;
-        alertPeriodInHours: number;
-        endTreatmentAt: Date | null;
-    }): Promise<Medication | null> {
-        return await this.prisma.medications.create({
-            data: {
-                ...newMedication
-            }
-        })
-    }
-
-    async updateMedication(id: string, updateMedication: {
-        treatmentId?: string | null;
-        name?: string | null;
-        dose?: string | null;
-        description?: string | null;
-        visualTypeId?: string | null;
-        soundTypeId?: string | null;
-        alertPeriodInHours?: number | null;
-        endTreatmentAt?: Date | null;
-    }): Promise<Medication | null> {
-        const { ...data } = updateMedication;
-        const updateData = Object.fromEntries(
-            Object.entries(data).filter(([_, v]) => v !== undefined)
-        );
-
-        return this.prisma.medications.update({
-            where: { medicationId: id },
-            data: updateData,
-        });
-    }
-
-
-    async deleteMedication(id: string): Promise<Medication | null> {
-        return this.prisma.medications.delete({
-            where: { medicationId: id },
-        });
-    }
-}
-
-export const defaultMedicationRepository = new PrismaMedicationRepository(prisma);
+export const defaultMedicationRepository = new PrismaMedicationRepository(
+  prisma,
+);
--- medallert-backend/src/services/medication-service.ts
+++ medallert-backend/src/services/medication-service.ts
@@ -1,5 +1,8 @@
 import z from "zod";
-import type { Medication, MedicationRepository } from "../repositories/medications.js";
+import type {
+  Medication,
+  MedicationRepository,
+} from "../repositories/medications.js";
 import type { PromiseResult } from "../common/type-helpers.js";
 import type { UsersRepository } from "../repositories/users.js";
 import { error, ok, t } from "try";
@@ -8,131 +11,162 @@ import type { SoundTypesRepository } from "../repositories/sound_types.js";
 import type { TreatmentRepository } from "../repositories/treatments.js";
 
 export const MedicationSchema = z.object({
-    name: z.string(),
-    dose: z.string().nullable().optional(),
-    description: z.string().nullable().optional(),
-    treatmentId: z.string().nullable().optional(),
-    visualTypeId: z.string(),
-    soundTypeId: z.string(),
-    alertPeriodInHours: z.number(),
-    endTreatmentAt: z.date().nullable().optional(),
-
-})
+  name: z.string(),
+  dose: z.string().nullable().optional(),
+  description: z.string().nullable().optional(),
+  treatmentId: z.string().nullable().optional(),
+  visualTypeId: z.string().nullable().optional(),
+  soundTypeId: z.string().nullable().optional(),
+  alertPeriodInHours: z.number(),
+  endTreatmentAt: z
+    .string()
+    .refine((s) => !Number.isNaN(Date.parse(s)), {
+      message: "Invalid ISO date",
+    })
+    .transform((s) => new Date(s))
+    .nullable()
+    .optional(),
+});
 
 export const MedicationIdParamSchema = z.object({
-    id: z.string().min(1, "ID is required"),
+  id: z.string().min(1, "ID is required"),
 });
 
 export const MedicationUpdateSchema = MedicationSchema.partial();
 
-export type MedicationType = z.infer<typeof MedicationSchema>
+export type MedicationType = z.infer<typeof MedicationSchema>;
 
 export class MedicationService {
-    constructor(
-        private readonly usersRepository: UsersRepository,
-        private readonly medicationRepository: MedicationRepository,
-        private readonly treatmentRepository: TreatmentRepository,
-        private readonly visualTypesRepository: VisualTypesRepository,
-        private readonly soundTypesRepository: SoundTypesRepository
-    ) { }
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly medicationRepository: MedicationRepository,
+    private readonly treatmentRepository: TreatmentRepository,
+    private readonly visualTypesRepository: VisualTypesRepository,
+    private readonly soundTypesRepository: SoundTypesRepository,
+  ) {}
 
-    async getAll(userId: string): PromiseResult<Medication[]> {
-        const medications = await this.medicationRepository.findAllMedications(userId);
+  async getAll(userId: string): PromiseResult<Medication[]> {
+    const medications =
+      await this.medicationRepository.findAllMedications(userId);
 
-        return ok(medications);
+    return ok(medications);
+  }
+
+  async get(medicationId: string): PromiseResult<Medication> {
+    const medication =
+      await this.medicationRepository.findMedication(medicationId);
+    if (!medication) return error("Medication not found");
+
+    return ok(medication);
+  }
+
+  async create(
+    userId: string,
+    {
+      name,
+      dose,
+      description,
+      treatmentId,
+      visualTypeId,
+      soundTypeId,
+      alertPeriodInHours,
+      endTreatmentAt,
+    }: MedicationType,
+  ): PromiseResult<Medication> {
+    const user = await this.usersRepository.findUser(userId);
+    if (!user) return error("User not found!");
+
+    if (treatmentId) {
+      const treatment =
+        await this.treatmentRepository.findTreatment(treatmentId);
+      if (!treatment) return error("Treatment not found");
     }
 
-    async get(medicationId: string): PromiseResult<Medication> {
-        const medication = await this.medicationRepository.findMedication(medicationId);
-        if (!medication) return error("Medication not found");
-
-        return ok(medication);
+    let visualType = null;
+    if (visualTypeId) {
+      visualType =
+        await this.visualTypesRepository.findVisualType(visualTypeId);
     }
 
-    async create(userId: string, {
+    let soundType = null;
+    if (soundTypeId) {
+      soundType = await this.soundTypesRepository.findSoundType(soundTypeId);
+    }
+
+    type OptionalId = string;
+
+    const [createdOk, _, createdMedication] = await t(
+      this.medicationRepository.addMedication({
+        userId,
         name,
-        dose,
-        description,
-        treatmentId,
-        visualTypeId,
-        soundTypeId,
+        dose: dose ?? null,
+        description: description ?? null,
+        visualTypeId: visualTypeId as OptionalId,
+        soundTypeId: soundTypeId as OptionalId,
         alertPeriodInHours,
-        endTreatmentAt,
-    }: MedicationType): PromiseResult<Medication> {
-        const user = await this.usersRepository.findUser(userId);
-        if (!user) return error("User not found!");
+        endTreatmentAt: endTreatmentAt ?? null,
+      }),
+    );
 
-        if (treatmentId) {
-            const treatment = await this.treatmentRepository.findTreatment(treatmentId);
-            if (!treatment) return error("Treatment not found");
-        }
+    if (!createdOk || !createdMedication)
+      return error("failed to create medication");
 
-        const visualType = await this.visualTypesRepository.findVisualType(visualTypeId);
-        if (!visualType) return error("Visual type not found");
+    return ok(createdMedication);
+  }
 
-        const soundType = await this.soundTypesRepository.findSoundType(soundTypeId);
-        if (!soundType) return error("Sound type not found");
+  async update(
+    medicationId: string,
+    updateData: Partial<MedicationType>,
+  ): PromiseResult<Medication> {
+    const medication =
+      await this.medicationRepository.findMedication(medicationId);
+    if (!medication) return error("Medication not found");
 
-        const [createdOk, _, createdMedication] = await t(
-            this.medicationRepository.addMedication({
-                userId,
-                name,
-                dose: dose ?? null,
-                description: description ?? null,
-                visualTypeId,
-                soundTypeId,
-                alertPeriodInHours,
-                endTreatmentAt: endTreatmentAt ?? null,
-            })
-        );
-
-        if (!createdOk || !createdMedication) return error("failed to create medication");
-
-        return ok(createdMedication);
+    if (updateData.treatmentId) {
+      const treatment = await this.treatmentRepository.findTreatment(
+        updateData.treatmentId,
+      );
+      if (!treatment) return error("Treatment not found");
     }
 
-    async update(
-        medicationId: string,
-        updateData: Partial<MedicationType>
-    ): PromiseResult<Medication> {
-        const medication = await this.medicationRepository.findMedication(medicationId);
-        if (!medication) return error("Medication not found");
-
-        if (updateData.treatmentId) {
-            const treatment = await this.treatmentRepository.findTreatment(updateData.treatmentId);
-            if (!treatment) return error("Treatment not found");
-        }
-
-        if (updateData.visualTypeId) {
-            const visualType = await this.visualTypesRepository.findVisualType(updateData.visualTypeId);
-            if (!visualType) return error("Visual type not found");
-        }
-
-        if (updateData.soundTypeId) {
-            const soundType = await this.soundTypesRepository.findSoundType(updateData.soundTypeId);
-            if (!soundType) return error("Sound type not found");
-        }
-
-        const [updatedOk, _, updatedMedication] = await t(
-            this.medicationRepository.updateMedication(medicationId, { ...updateData })
-        );
-
-        if (!updatedOk || !updatedMedication) return error("Failed to update medication");
-
-        return ok(updatedMedication);
+    if (updateData.visualTypeId) {
+      const visualType = await this.visualTypesRepository.findVisualType(
+        updateData.visualTypeId,
+      );
+      if (!visualType) return error("Visual type not found");
     }
 
-    async delete(medicationId: string): PromiseResult<Medication> {
-        const medication = await this.medicationRepository.findMedication(medicationId);
-        if (!medication) return error("Medication not found");
-
-        const [deletedOk, _, deletedMedication] = await t(
-            this.medicationRepository.deleteMedication(medicationId)
-        );
-
-        if (!deletedOk || !deletedMedication) return error("Failed to delete medication");
-
-
-        return ok(deletedMedication);
+    if (updateData.soundTypeId) {
+      const soundType = await this.soundTypesRepository.findSoundType(
+        updateData.soundTypeId,
+      );
+      if (!soundType) return error("Sound type not found");
     }
+
+    const [updatedOk, _, updatedMedication] = await t(
+      this.medicationRepository.updateMedication(medicationId, {
+        ...updateData,
+      }),
+    );
+
+    if (!updatedOk || !updatedMedication)
+      return error("Failed to update medication");
+
+    return ok(updatedMedication);
+  }
+
+  async delete(medicationId: string): PromiseResult<Medication> {
+    const medication =
+      await this.medicationRepository.findMedication(medicationId);
+    if (!medication) return error("Medication not found");
+
+    const [deletedOk, _, deletedMedication] = await t(
+      this.medicationRepository.deleteMedication(medicationId),
+    );
+
+    if (!deletedOk || !deletedMedication)
+      return error("Failed to delete medication");
+
+    return ok(deletedMedication);
+  }
 }
